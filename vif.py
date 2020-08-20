3af95d8eb55eb07ae3a282c9df9b337fb66b58e2
Todor <tvelichkovt@gmail.com>
2020-08-20 18:12:02 +0300

    Create vif.py

---
+++ vif.py
@@ -0,0 +1,28 @@
+# Detecting Multicollinearity using VIF
+import pandas as pd
+
+df=pd.read_csv('salary.csv') ; print(df.head())
+
+
+
+# Import library for VIF
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+def calc_vif(X):
+
+    # Calculating VIF
+    vif = pd.DataFrame()
+    vif["variables"] = X.columns
+    vif["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+
+    return(vif)
+
+# calc_vif(df)
+
+print(calc_vif(df))
+
+# FIX calc_vif(df)
+
+X = df.drop(['exp','age'],axis=1)
+print(calc_vif(X))
+df.to_csv('out.csv', sep='\t')
