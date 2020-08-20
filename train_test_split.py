35e261923425aa096fbf2e286ecd657d7ce9dc97
Todor <tvelichkovt@gmail.com>
2020-08-20 19:03:59 +0300

    Create train_test_split.py

---
+++ train_test_split.py
@@ -0,0 +1,102 @@
+#train_test_split
+import pandas as pd
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_boston
+import numpy as np
+
+# Load the Diabetes dataset
+columns = 'age sex bmi map tc ldl hdl tch ltg glu'.split() # Declare the columns names
+diabetes = datasets.load_diabetes() # Call the diabetes dataset from sklearn
+df = pd.DataFrame(diabetes.data, columns=columns) # load the dataset as a pandas data frame
+y = diabetes.target # define the target variable (dependent variable) as y
+
+# create training and testing vars
+X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+# fit a model
+lm = linear_model.LinearRegression()
+model = lm.fit(X_train, y_train)
+predictions = lm.predict(X_test)
+
+# Let’s see what (some of) the predictions are:
+
+print(predictions[0:5])
+
+
+# plot the model
+plt.scatter(y_test, predictions)
+plt.xlabel('True Values')
+plt.ylabel('Predictions')
+
+#print the accuracy score:
+print(model.score(X_test, y_test))
+
+# K-Folds Cross Validation
+
+from sklearn.model_selection import KFold # import KFold
+X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]]) # create an array
+y = np.array([1, 2, 3, 4]) # Create another array
+kf = KFold(n_splits=2) # Define the split - into 2 folds 
+kf.get_n_splits(X) # returns the number of splitting iterations in the cross-validator
+print(kf) 
+KFold(n_splits=2, random_state=None, shuffle=False)
+
+for train_index, test_index in kf.split(X):
+ print('TRAIN:', train_index, 'TEST:', test_index)
+ X_train, X_test = X[train_index], X[test_index]
+ y_train, y_test = y[train_index], y[test_index]
+('TRAIN:', array([2, 3]), 'TEST:', array([0, 1]))
+('TRAIN:', array([0, 1]), 'TEST:', array([2, 3]))
+
+
+
+# Leave One Out Cross Validation (LOOCV)
+
+from sklearn.model_selection import LeaveOneOut 
+X = np.array([[1, 2], [3, 4]])
+y = np.array([1, 2])
+loo = LeaveOneOut()
+loo.get_n_splits(X)
+
+
+for train_index, test_index in loo.split(X):
+   print("TRAIN:", train_index, "TEST:", test_index)
+   X_train, X_test = X[train_index], X[test_index]
+   y_train, y_test = y[train_index], y[test_index]
+   print(X_train, X_test, y_train, y_test)
+
+# Necessary imports: 
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn import metrics
+
+# Perform 6-fold cross validation
+scores = cross_val_score(model, df, y, cv=6)
+print('Cross-validated scores:', scores)
+
+
+# Make cross validated predictions
+from sklearn.model_selection import cross_val_predict
+
+predictions = cross_val_predict(model, df, y, cv=6)
+plt.scatter(y, predictions)
+
+accuracy = metrics.r2_score(y, predictions)
+print('Cross-Predicted Accuracy:', accuracy)
+
+https://towardsdatascience.com/train-test-split-and-cross-validation-in-python-80b61beca4b6
+
+
+
+
+
+
+
+
+
+
+
+
