5cb91306820b5c4a50e505da32e1a290785e6243
Todor <tvelichkovt@gmail.com>
2020-08-20 20:07:31 +0300

    Update vif.py

--- vif.py
+++ vif.py
@@ -1,13 +1,9 @@
 # Detecting Multicollinearity using VIF
 import pandas as pd
-
 df = pd.read_csv('salary_small.csv') ; print(df.head())
-
-
 
 # Import library for VIF
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-
 def calc_vif(X):
 
     # Calculating VIF
@@ -18,11 +14,8 @@ def calc_vif(X):
     return(vif)
 
 # calc_vif(df)
-
 print(calc_vif(df)) ; #experience and age are over-correlated >> 5 !!!
 
 # FIX calc_vif(df)
-
 X = df.drop(['exp','age'],axis=1)
-print(calc_vif(X))
-# df.to_csv('out.csv')
+print(calc_vif(X)) # df.to_csv('out.csv')
